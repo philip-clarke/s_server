54303bf96a7cd8c97ece1d059d79491066049811
Philip Clarke <send2philip@gmail.com>
2011-11-23 21:56:07 +0000

    added a second test which should fail, but doesn't ???

--- src/s_server_tests.erl
+++ src/s_server_tests.erl
@@ -1,12 +1,18 @@
 -module(s_server_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(setup(F), {setup, fun start/0, fun stop/1, F}).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% TESTS DESCRIPTIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 start_stop_test() ->
     {"Test that the server can be started, stopped and has a registered name",
-        {setup, fun start/0, fun stop/1, fun is_registered/1}}.
+    ?setup(fun is_registered/1)}.
+
+ping_test() ->
+    {"Test that the response to ping is pong",
+    ?setup(fun response_with_pong/1)}.
  
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% SETUP FUNCTIONS %%%
@@ -24,6 +30,9 @@ stop(_Pid) ->
 is_registered(Pid) ->
     [?_assert(erlang:is_process_alive(Pid)),
      ?_assertEqual(Pid, whereis(s_server))].
+
+response_with_pong(_Pid) ->
+    [?_assertEqual(pang, s_server:ping())].
  
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%% HELPER FUNCTIONS %%%
